1c65104228b901a710f23b767dfd59f039007100
PastelThingss <ylvara@hiof.no>
2026-05-13 00:27:40 +0200

    added a "big" update/chnage
    Added the js for the image carousel for utvalgte prosjekter and the html and css needed with it. Its in testing fase and chnages may occur

---
+++ javascript.js
@@ -0,0 +1,22 @@
+function changeSlide(btn, direction) {
+    //finn den bilde carouselen denne knappen er med/tilhører til
+    const carousel = btn.closest('.carousel');
+    //bilder som er i kortet og tellern hvor mange bilder er det
+    const imgs = carousel.querySelectorAll(`.carousel-images img`);
+    const counter = carousel.querySelector('.carousel-counter');
+
+    //finn hvilket bilde som vises nå altså aktivt vises på skjeremn
+    let current = 0;
+    imgs.forEach((img, i) => {
+        if (!img.classList.contains(`hidden`)) current = i;
+    });
+
+    //skul/gjem nåværende bilde og vis neste bilde
+    imgs[current].classList.add(`hidden`);
+    let next = (current + direction + imgs.length) % imgs.length;
+    imgs[next].classList.remove(`hidden`);
+
+    //oppdater teller tallet
+    counter.textContent = (next + 1) + ` /` + imgs.length;
+
+}
